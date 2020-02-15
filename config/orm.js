c703195e46183fddc511107544b1138136a5a20e
Sia <ashkiani@yahoo.com>
2020-02-15 10:26:31 -0500

    Initial orm

---
+++ config/orm.js
@@ -0,0 +1,71 @@
+var connection = require("../config/connection.js");
+
+function printQuestionMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
+var orm = {
+    selectAll: function (table, cb) {
+        connection.query(`SELECT * FROM ${table};`, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne: function (table, cols, vals, cb) {
+        var queryString = `INSERT INTO ${table} (${cols.toString()}) VALUES (${printQuestionMarks(vals.length)})`;
+        console.log(queryString);
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err
+            }
+            cb(result);
+        });
+    },
+    updateOne: function (table, objColVals, condition, cb) {
+        var queryString = `UPDATE ${table} SET ${objToSql(objColVals)} WHERE ${condition}`
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err
+            }
+            cb(result);
+        });
+    },
+    deleteOne: function (table, condition, cb) {
+        connection.query(`DELETE FROM ${table} WHERE ${condition}`, function (err, result) {
+            if (err) {
+                throw err
+            }
+            cb(result);
+        });
+    }
+};
+
+module.exports = orm;
