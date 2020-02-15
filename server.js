5e40f12fe5c8e3823a7dadc6d41eeade4bec5f02
Sia <ashkiani@yahoo.com>
2020-02-15 13:50:42 -0500

    updated the server to enable the routes

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
+var bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
